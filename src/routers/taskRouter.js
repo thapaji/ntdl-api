7c23d1609b822d7ff22fdf868cfeebf23eb02f5e
Sujan <mail.thapasujan@gmail.com>
2024-04-22 10:53:01 +1000

    Code refactor and performance

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -4,8 +4,6 @@ import { insertTask, getTasks, updateTask, deleteTask } from "../models/task/Tas
 
 const router = express.Router();
 
-let fakeDb = [];
-
 /* GET*/
 router.get("/", async (req, res) => {
   const result = await getTasks();
@@ -31,49 +29,45 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  /*
-    const id = idGenerator();
-    console.log(req.body);
-    fakeDb.push({ ...req.body, id });
-  */
 });
 
 /*update task*/
 router.patch("/", async (req, res) => {
-  const { id, type } = req.body;
-  const result = await updateTask(id, type);
-  // console.log(result);
-
-  // fakeDb = fakeDb.map((item) => {
-  //   if (item.id === id) {
-  //     return { ...item, type };
-  //   }
-  //   return item;
-  // });
-  result?.acknowledged
-    ? res.json({
-        message: "Your task has been updated",
-      })
-    : res.json({
-        message: "Your task could not be updated",
-      });
+  try {
+    const result = await updateTask(req.body);
+    result?._id
+      ? res.json({
+          message: "Your task has been updated",
+        })
+      : res.json({
+          message: "Your task could not be updated",
+        });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong in server. Please contact the provider.",
+    });
+  }
 });
 
 /*delete task*/
 router.delete("/", async (req, res) => {
-  const { id } = req.body;
-  const result = await deleteTask(id);
+  try {
+    console.log(req.body);
+    const { id } = req.body;
+    const result = await deleteTask(id);
 
-  // console.log(id);
-  // fakeDb = fakeDb.filter((item) => item.id !== id);
-
-  result?.acknowledged
-    ? res.json({
-        message: "Your task has been deleted",
-      })
-    : res.json({
-        message: "Your task could not be deleted",
-      });
+    result?._id
+      ? res.json({
+          message: "Your task has been deleted",
+        })
+      : res.json({
+          message: "Your task could not be deleted",
+        });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong in server. Please contact the provider.",
+    });
+  }
 });
 
 export default router;
